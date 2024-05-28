1208a8ba612c8fb73932694f3871063b32fef843
Zachary Polof <ZacharyPolof@gmail.com>
2024-05-27 19:54:44 -0500

    server.js
    added final dependency
    
    Server: set project required protocols.
    
    setup express
    
    setup express middlware

---
+++ server.js
@@ -0,0 +1,13 @@
+//set project requirements
+const inquirer = require('inquirer');
+const express = require('express');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+//set up express
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
