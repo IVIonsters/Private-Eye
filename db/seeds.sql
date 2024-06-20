346847d1c0aee3ea23a68d2bd37e7084441b587a
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-06-19 22:59:45 -0500

    Update seeds.sql
    created seeds file with mock data for filling tables

---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+\ c privateeyeDB 
+
+-- Insert departments
+INSERT INTO department (name)
+VALUES ('Service'),
+    ('Sales'),
+    ('HR'),
+    ('IT');
+
+-- Insert roles
+INSERT INTO role(title, salary, department_id)
+VALUES ('Manager', 65000, 1),
+    ('Service Advisor', 50000, 1),
+    ('Sales Manager', 68000, 2),
+    ('Customer Representative', 45000, 2),
+    ('HR Manager', 105000, 3),
+    ('Employee Aid', 55000, 3),
+    ('Senior Developer', 205000, 4),
+    ('Junior Developer', 95000, 4),
+
+-- Insert employees
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Brown', 1, NULL),
+    ('Jane', 'Red', 1, 1),
+    ('Kenny', 'Hope', 2, NULL),
+    ('Jill', 'Novak', 2, 3),
+    ('Alex', 'Smith', 3, NULL),
+    ('Mark', 'Coast', 3, 5),
+    ('Brandon', 'silver', 4, NULL),
+    ('Brock', 'Beechum', 4, 7);
