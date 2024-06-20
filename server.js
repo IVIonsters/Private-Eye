66a713b712ac907f4bf775a42ad9777120a45526
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-06-19 22:26:44 -0500

    Update server.js
    setup pseudo code for functions needed on server.js

--- server.js
+++ server.js
@@ -11,3 +11,38 @@ const PORT = process.env.PORT || 3000;
 //express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//set up database connection
+
+
+//connect to database
+
+
+//function for menu options
+
+
+//function to view all departments
+
+
+//function to view all roles
+
+
+//function to view all employees
+
+
+//function to add a department
+
+
+//function to add a role
+
+
+//function to add an employee
+
+
+//function to update an employee role
+
+
+//port listener
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
